a584f55a317eeb0fe8a467f629bce45c5e8a0746
Tyler Rhodes <rhodetyl000@gmail.com>
2021-07-26 23:33:36 -0500

    Format index page for packages

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,40 @@
 Product.delete_all
 #
-Product.create!(title: 'Product 1', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa obcaecati modi sint suscipit delectus impedit officiis quam velit, architecto minus dolorum, fuga quia numquam accusamus mollitia totam sunt aspernatur deserunt cupiditate nulla perspiciatis debitis voluptatem. Maiores asperiores non, minus quibusdam architecto quos, rem explicabo, aliquam eligendi eum magnam quidem velit totam ipsam! Non, incidunt recusandae libero illum voluptatum pariatur atque suscipit necessitatibus fuga consequuntur eaque, veniam iusto! Quis exercitationem veniam doloremque id odit officia, laborum voluptatem iure aut debitis cum tempore enim ipsam tenetur magni eum nostrum consequatur provident molestias laboriosam tempora nemo nesciunt! Facilis, delectus. Quia consequuntur explicabo minus?', price: '10.00')
-Product.create!(title: 'Product 2', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa obcaecati modi sint suscipit delectus impedit officiis quam velit, architecto minus dolorum, fuga quia numquam accusamus mollitia totam sunt aspernatur deserunt cupiditate nulla perspiciatis debitis voluptatem. Maiores asperiores non, minus quibusdam architecto quos, rem explicabo, aliquam eligendi eum magnam quidem velit totam ipsam! Non, incidunt recusandae libero illum voluptatum pariatur atque suscipit necessitatibus fuga consequuntur eaque, veniam iusto! Quis exercitationem veniam doloremque id odit officia, laborum voluptatem iure aut debitis cum tempore enim ipsam tenetur magni eum nostrum consequatur provident molestias laboriosam tempora nemo nesciunt! Facilis, delectus. Quia consequuntur explicabo minus?', price: '20.00') 
-Product.create!(title: 'Product 3', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa obcaecati modi sint suscipit delectus impedit officiis quam velit, architecto minus dolorum, fuga quia numquam accusamus mollitia totam sunt aspernatur deserunt cupiditate nulla perspiciatis debitis voluptatem. Maiores asperiores non, minus quibusdam architecto quos, rem explicabo, aliquam eligendi eum magnam quidem velit totam ipsam! Non, incidunt recusandae libero illum voluptatum pariatur atque suscipit necessitatibus fuga consequuntur eaque, veniam iusto! Quis exercitationem veniam doloremque id odit officia, laborum voluptatem iure aut debitis cum tempore enim ipsam tenetur magni eum nostrum consequatur provident molestias laboriosam tempora nemo nesciunt! Facilis, delectus. Quia consequuntur explicabo minus?', price: '30.00')
+Product.create!(title: 'Product 1', description: 'Lorem ipsum dolor sit amet,
+consectetur adipisicing elit. Culpa obcaecati modi sint suscipit delectus
+impedit officiis quam velit, architecto minus dolorum, fuga quia numquam
+accusamus mollitia totam sunt aspernatur deserunt cupiditate nulla perspiciatis
+debitis voluptatem. Maiores asperiores non, minus quibusdam architecto quos, rem
+explicabo, aliquam eligendi eum magnam quidem velit totam ipsam! Non, incidunt
+recusandae libero illum voluptatum pariatur atque suscipit necessitatibus fuga
+consequuntur eaque, veniam iusto! Quis exercitationem veniam doloremque id odit
+officia, laborum voluptatem iure aut debitis cum tempore enim ipsam tenetur
+magni eum nostrum consequatur provident molestias laboriosam tempora nemo
+nesciunt! Facilis, delectus. Quia consequuntur explicabo minus?', price:
+'10.00', image_url: 'http://www.placehold.it/350x150/EFEFEF/AAAAAA&text=Product+1')
+Product.create!(title: 'Product 2', description: 'Lorem ipsum dolor sit amet,
+consectetur adipisicing elit. Culpa obcaecati modi sint suscipit delectus
+impedit officiis quam velit, architecto minus dolorum, fuga quia numquam
+accusamus mollitia totam sunt aspernatur deserunt cupiditate nulla perspiciatis
+debitis voluptatem. Maiores asperiores non, minus quibusdam architecto quos, rem
+explicabo, aliquam eligendi eum magnam quidem velit totam ipsam! Non, incidunt
+recusandae libero illum voluptatum pariatur atque suscipit necessitatibus fuga
+consequuntur eaque, veniam iusto! Quis exercitationem veniam doloremque id odit
+officia, laborum voluptatem iure aut debitis cum tempore enim ipsam tenetur
+magni eum nostrum consequatur provident molestias laboriosam tempora nemo
+nesciunt! Facilis, delectus. Quia consequuntur explicabo minus?', price:
+'20.00', image_url:
+'http://www.placehold.it/350x150/EFEFEF/AAAAAA&text=Product+2') 
+Product.create!(title: 'Product 3', description: 'Lorem ipsum dolor sit amet,
+consectetur adipisicing elit. Culpa obcaecati modi sint suscipit delectus
+impedit officiis quam velit, architecto minus dolorum, fuga quia numquam
+accusamus mollitia totam sunt aspernatur deserunt cupiditate nulla perspiciatis
+debitis voluptatem. Maiores asperiores non, minus quibusdam architecto quos, rem
+explicabo, aliquam eligendi eum magnam quidem velit totam ipsam! Non, incidunt
+recusandae libero illum voluptatum pariatur atque suscipit necessitatibus fuga
+consequuntur eaque, veniam iusto! Quis exercitationem veniam doloremque id odit
+officia, laborum voluptatem iure aut debitis cum tempore enim ipsam tenetur
+magni eum nostrum consequatur provident molestias laboriosam tempora nemo
+nesciunt! Facilis, delectus. Quia consequuntur explicabo minus?', price:
+'30.00', image_url:
+'http://www.placehold.it/350x150/EFEFEF/AAAAAA&text=Product+3')
